ae8eaba1b982fac244f2cf9dd959741db9f29707
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-04-30 19:51:15 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
 
     const embed = new Discord.MessageEmbed()
     .setAuthor(`${client.user.username} - Invite`, client.user.avatarURL())
-    .setDescription(`<a:vp_ann:814395137945239552> | ${await client.translate(`You can invite.`, message)} **${client.user.username}** **[${await client.translate(`here.`, message)}](https://discord.com/api/oauth2/authorize?client_id=808336313681575986&permissions=8&scope=bot)**.`)
+    .setDescription(`<a:vp_ann:814395137945239552> | ${await client.translate(`You can invite.`, message)} **${client.user.username}** **[${await client.translate(`here.`, message)}](https://discord.com/api/oauth2/authorize?client_id=804068017075257374&permissions=8&scope=bot)**.`)
     .setColor(message.guild.me.displayHexColor)
     .setTimestamp()
     .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
